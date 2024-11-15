ea5a55beb2f92c14be0bc431d69324282b36a4d1
gakuyuparam <hibiki-iwamot@gr.paramix.co.jp>
2024-11-15 16:50:42 +0900

    課題１０

---
+++ kadai_010/summary.js
@@ -0,0 +1,17 @@
+$(function () {
+
+  $('#fade-out').on('click',function() {
+    $('#target').fadeOut();
+  });
+  $('#fade-in').on('click',function() {
+    $('#target').fadeIn();
+  });
+  $('#change-color').on('click',function() {
+    
+    $('#target').css('color', 'red');
+  });
+  $('#change-text').on('click',function() {
+    
+    $('#target').text("Hello!");
+  });
+});
